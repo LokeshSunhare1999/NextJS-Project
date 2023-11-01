6018e853eb221ceca64560c9c358672aefd51601
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2023-11-01 23:59:09 +0530

    first portfolio commit

---
+++ my-portfolio/src/app/components/HeroSection.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const HeroSection = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 lg:grid-cols-12">
+        <div className="col-span-7 place-self-center">
+          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+            Hello, I'm Lokesh
+          </h1>
+          <p className="text-[#ADB7BE] text-lg lg:text-xl">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially u
+          </p>
+        </div>
+        <div className="col-span-5"></div>
+      </div>
+    </section>
+  );
+};
