155321a7ddc760cbc20eb3eadc46c10ac64d249f
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2023-11-02 23:44:10 +0530

    creating home page

--- my-portfolio/src/app/components/HeroSection.js
+++ my-portfolio/src/app/components/HeroSection.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 export const HeroSection = () => {
   return (
@@ -11,13 +12,28 @@ export const HeroSection = () => {
           <p className="text-[#ADB7BE] text-lg lg:text-xl">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially u
+            ever since the 1500s, when an unknown printer took a galley
           </p>
+          <div>
+            <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">
+              Hire Me
+            </button>
+            <button className="px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-4">
+              Download CV
+            </button>
+          </div>
         </div>
-        <div className="col-span-5"></div>
+        <div className="col-span-5 place-self-center mt-4 lg:text-xl">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/hero-image.png"
+              alt="hero image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
