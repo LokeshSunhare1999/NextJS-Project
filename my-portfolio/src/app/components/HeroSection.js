369ea633e63131aa4033363a0a47fff4e84f0839
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2023-11-03 00:11:36 +0530

    added type animation

--- my-portfolio/src/app/components/HeroSection.js
+++ my-portfolio/src/app/components/HeroSection.js
@@ -1,25 +1,47 @@
+"use client";
 import React from "react";
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 
 export const HeroSection = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7 place-self-center">
-          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-            Hello, I'm Lokesh
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              Hello, I'm{" "}
+            </span>
+            <br />
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "Lokesh",
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                "Web Devloper",
+                1000,
+                "Next.js Devloper",
+                1000,
+                "React.js Devloper",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              style={{ fontSize: "2em", display: "inline-block" }}
+              repeat={Infinity}
+            />
           </h1>
-          <p className="text-[#ADB7BE] text-lg lg:text-xl">
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley
           </p>
           <div>
-            <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-black">
               Hire Me
             </button>
-            <button className="px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-4">
-              Download CV
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-transparent bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-4">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
             </button>
           </div>
         </div>
