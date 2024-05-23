01f2e7e99b768cc38a690f3e4dca5a3168696aa0
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2024-05-23 23:52:58 +0530

    Worked on auth module

--- banking-app/app/(auth)/sign-in/page.tsx
+++ banking-app/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,12 @@
+import AuthForm from "@/components/ui/AuthForm";
 import React from "react";
 
-function SignIn() {
-  return <div>SignIn</div>;
-}
+const SignIn = () => {
+  return (
+    <section className="flex-center size-full max-sm:px-6 ">
+      <AuthForm type="sign-in" />
+    </section>
+  );
+};
 
 export default SignIn;
---
+++ banking-app/components/ui/AuthForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AuthForm = ({ type }: { type: string }) => {
+  return <div>{type}</div>;
+};
+
+export default AuthForm;
