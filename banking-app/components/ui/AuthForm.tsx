80d9bf8d436bf5943c54477390af6eb3f68ec730
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2024-05-29 23:54:50 +0530

    removed unused code

--- banking-app/components/ui/AuthForm.tsx
+++ banking-app/components/ui/AuthForm.tsx
@@ -7,7 +7,6 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
 
