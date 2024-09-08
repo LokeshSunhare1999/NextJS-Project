2ae87185d4567df48599119e0ab00765bae9429d
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2024-09-08 19:12:11 +0530

    make to work nav links

--- my-portfolio/src/app/components/ProjectSection.js
+++ my-portfolio/src/app/components/ProjectSection.js
@@ -44,7 +44,7 @@ const ProjectSection = () => {
     animate: { y: 0, opacity: 1 },
   };
   return (
-    <section ref={ref}>
+    <section ref={ref} id="projects">
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
         My Projects
       </h2>
