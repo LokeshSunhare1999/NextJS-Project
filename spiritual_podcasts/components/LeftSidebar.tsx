c43e2a13b4fccdd9331155de4e0ec76d97325f1d
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2025-02-22 23:34:54 +0530

    added assets and LeftSidebar component

--- spiritual_podcasts/components/LeftSidebar.tsx
+++ spiritual_podcasts/components/LeftSidebar.tsx
@@ -1,15 +1,33 @@
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import { sidebarLinks } from "@/constants";
 
 const LeftSidebar = () => {
   return (
     <section className="left_sidebar">
       <nav className="flex flex-col gap-6 ">
-        <Link href="/">
-          <Image src="" alt="" />
+        <Link
+          href="/"
+          className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center"
+        >
+          <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
+          <h1 className="text-24 font-extrabold text-white max-lg:hidden">
+            Podcastr
+          </h1>
         </Link>
-        sdfsd
+        {sidebarLinks.map(({ route, label, imgURL }) => {
+          return (
+            <Link
+              key={label}
+              href={route}
+              className="flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start"
+            >
+              <Image src={imgURL} alt={label} width={24} height={24} />
+              <p>{label}</p>
+            </Link>
+          );
+        })}
       </nav>
     </section>
   );
