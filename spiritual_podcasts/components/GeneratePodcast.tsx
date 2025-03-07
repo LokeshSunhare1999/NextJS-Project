d4a60b290fbd17ddf041cdee518dce200afa58c0
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2025-03-08 00:26:20 +0530

    start work on openai integration

--- spiritual_podcasts/components/GeneratePodcast.tsx
+++ spiritual_podcasts/components/GeneratePodcast.tsx
@@ -1,39 +1,84 @@
-import { GeneratePodcastProps } from '@/types'
-import React, { useState } from 'react'
-import { Label } from './ui/label'
-import { Textarea } from './ui/textarea'
-import { Loader } from 'lucide-react'
-import { Button } from './ui/button'
+import { GeneratePodcastProps } from "@/types";
+import React, { useState } from "react";
+import { Label } from "./ui/label";
+import { Textarea } from "./ui/textarea";
+import { Loader } from "lucide-react";
+import { Button } from "./ui/button";
+import { set } from "react-hook-form";
 
-const useGeneratePodcast = (props: GeneratePodcastProps) => {
-    // logic for generating podcast
-return {
-    isGenerating: false,
-    generatePodcasts: () => {}
-}
-}
+const useGeneratePodcast = ({
+  setAudio,
+  voiceType,
+  voicePrompt,
+  setAudioStorageId,
+}: GeneratePodcastProps) => {
+  const [isGenerating, setIsGenerating] = useState(false);
+  // logic for generating podcast
+  const generatePodcasts = async () => {
+    setIsGenerating(true);
+    setAudio("");
+
+    if (!voicePrompt) {
+      return setIsGenerating(false);
+    }
+
+    try {
+      //   const response = await getPodcastAudio({
+      //     voice: voiceType,
+      //     input: voicePrompt,
+      //   });
+    } catch (error) {
+      console.log(error);
+      setIsGenerating(false);
+    }
+  };
+  return { isGenerating, generatePodcasts };
+};
 
 const GeneratePodcast = (props: GeneratePodcastProps) => {
-    const {isGenerating, generatePodcasts} = useGeneratePodcast(props);
-    
-    return (
-        <div>
-            <div className="flex flex-col gap-2.5">
-                <Label className='text-16 font-bold text-white-1'>Ai Prompt to generate Podcast</Label>
-                <Textarea className='input-class font-light focus-visible:ring-offset-orange-1'
-                    placeholder='Provide text to generate audio'
-                    rows={5}
-                    value={props.voicePrompt}
-                    onChange={(e) => props.setVoicePrompt(e.target.value)} />
-            </div>
-            <div className="mt-5 w-full max-w-[200px]">
-                <Button type="submit" className="text-16 w-full bg-orange-1 py-4 font-bold text-white-1">{isGenerating ? (<>Generating <Loader size={20} className="animate-spin" /></>) : ("Generate")}</Button>
-            </div>
-            {props.audio && (
-                <audio controls src={props.audio} autoPlay className="mt-5" onLoadedMetadata={(e) => props.setAudioDuration(e.currentTarget.duration)} />
-            )}
-        </div>
-    )
-}
+  const { isGenerating, generatePodcasts } = useGeneratePodcast(props);
 
-export default GeneratePodcast
+  return (
+    <div>
+      <div className="flex flex-col gap-2.5">
+        <Label className="text-16 font-bold text-white-1">
+          Ai Prompt to generate Podcast
+        </Label>
+        <Textarea
+          className="input-class font-light focus-visible:ring-offset-orange-1"
+          placeholder="Provide text to generate audio"
+          rows={5}
+          value={props.voicePrompt}
+          onChange={(e) => props.setVoicePrompt(e.target.value)}
+        />
+      </div>
+      <div className="mt-5 w-full max-w-[200px]">
+        <Button
+          type="submit"
+          className="text-16 w-full bg-orange-1 py-4 font-bold text-white-1"
+        >
+          {isGenerating ? (
+            <>
+              Generating <Loader size={20} className="animate-spin" />
+            </>
+          ) : (
+            "Generate"
+          )}
+        </Button>
+      </div>
+      {props.audio && (
+        <audio
+          controls
+          src={props.audio}
+          autoPlay
+          className="mt-5"
+          onLoadedMetadata={(e) =>
+            props.setAudioDuration(e.currentTarget.duration)
+          }
+        />
+      )}
+    </div>
+  );
+};
+
+export default GeneratePodcast;
