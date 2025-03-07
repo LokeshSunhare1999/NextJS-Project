e47659f278eb16c0fefc6c1aff8ac95599edb9cd
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2025-03-07 23:52:55 +0530

    added GeneratePodcast & GenerateThumbnail

--- spiritual_podcasts/app/(root)/create-podcast/page.tsx
+++ spiritual_podcasts/app/(root)/create-podcast/page.tsx
@@ -23,23 +23,42 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
 import { useState } from "react";
+import { Textarea } from "@/components/ui/textarea";
+import GeneratePodcast from "@/components/GeneratePodcast";
+import GenerateThumbnail from "@/components/GenerateThumbnail";
+import { Loader } from "lucide-react";
+import { Id } from "@/convex/_generated/dataModel";
 
 const voiceCategories = ["alloy", "shimmer", "nova", "echo", "fable", "onyx"];
 
 const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
+  podcastTitle: z.string().min(2),
+  podcastDescription: z.string().min(2),
 });
 
 const CreatePodcast = () => {
+  const [imagePrompt, setImagePrompt] = useState("");
+  const [imageStorageId, setImageStorageId] = useState<Id<"_storage"> | null>(
+    null
+  );
+  const [imageUrl, setImageUrl] = useState("");
+  const [audioUrl, setAudioUrl] = useState("");
+  const [audioStorageId, setAudioStorageId] = useState<Id<"_storage"> | null>(
+    null
+  );
+  const [audioDuration, setAudioDuration] = useState(0);
+
+  const [voicePrompt, setVoicePrompt] = useState("");
   const [voiceType, setVoiceType] = useState<string | null>(null);
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      podcastTitle: "",
+      podcastDescription: "",
     },
   });
 
@@ -66,16 +85,15 @@ const CreatePodcast = () => {
               render={({ field }) => (
                 <FormItem className="flex flex-col gap-2.5">
                   <FormLabel className="text-16 font-bold text-white-1">
-                    Username
+                    Title
                   </FormLabel>
                   <FormControl>
                     <Input
-                      className="input-class focus-visible:ring-orange-1"
+                      className="input-class focus-visible:ring-offset-orange-1"
                       placeholder="Enter your podcasts title"
                       {...field}
                     />
                   </FormControl>
-
                   <FormMessage className="text-white-1" />
                 </FormItem>
               )}
@@ -84,7 +102,7 @@ const CreatePodcast = () => {
               <Label className="text-16 font-bold text-white-1">
                 Select Ai category
               </Label>
-              <Select>
+              <Select onValueChange={(value) => setVoiceType(value)}>
                 <SelectTrigger
                   className={cn(
                     "text-16 w-full border-none bg-black-1 text-gray-1"
@@ -95,12 +113,12 @@ const CreatePodcast = () => {
                     className="placeholder:text-gray-1"
                   />
                 </SelectTrigger>
-                <SelectContent className="text-16 border-none bg-black-1 font-bold text-white-1 focus:ring-orange-1">
+                <SelectContent className="text-16 border-none bg-black-1 font-bold text-white-1 focus-visible:ring-offset-orange-1">
                   {voiceCategories.map((category) => (
                     <SelectItem
                       key={category}
                       value={category}
-                      className="focus:bg-orange-1"
+                      className="focus-visible:ring-offset-orange-1"
                     >
                       {category}
                     </SelectItem>
@@ -115,6 +133,51 @@ const CreatePodcast = () => {
                   />
                 )}
               </Select>
+            </div>
+            <FormField
+              control={form.control}
+              name="podcastDescription"
+              render={({ field }) => (
+                <FormItem className="flex flex-col gap-2.5">
+                  <FormLabel className="text-16 font-bold text-white-1">
+                    Description
+                  </FormLabel>
+                  <FormControl>
+                    <Textarea
+                      className="input-class focus-visible:ring-offset-orange-1"
+                      placeholder="Write a short description for your podcasts"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className="text-white-1" />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="flex flex-col pt-10">
+            <GeneratePodcast
+              setAudioStorageId={setAudioStorageId}
+              setAudio={setAudioUrl}
+              viceType={voiceType}
+              audio={audioUrl}
+              voicePrompt={voicePrompt}
+              setVoicePrompt={setVoicePrompt}
+              setAudioDuration={setAudioDuration}
+            />
+            <GenerateThumbnail />
+            <div className="mt-10 w-full">
+              <Button
+                type="submit"
+                className="text-16 w-full bg-orange-1 py-4 font-extrabold text-white-1 transition-all duration-500 hover:bg-black-1"
+              >
+                {isSubmitting ? (
+                  <>
+                    Submitting <Loader size={20} className="animate-spin" />
+                  </>
+                ) : (
+                  "Submit & Publish Podcast"
+                )}
+              </Button>
             </div>
           </div>
         </form>
---
+++ spiritual_podcasts/components/GeneratePodcast.tsx
@@ -0,0 +1,39 @@
+import { GeneratePodcastProps } from '@/types'
+import React, { useState } from 'react'
+import { Label } from './ui/label'
+import { Textarea } from './ui/textarea'
+import { Loader } from 'lucide-react'
+import { Button } from './ui/button'
+
+const useGeneratePodcast = (props: GeneratePodcastProps) => {
+    // logic for generating podcast
+return {
+    isGenerating: false,
+    generatePodcasts: () => {}
+}
+}
+
+const GeneratePodcast = (props: GeneratePodcastProps) => {
+    const {isGenerating, generatePodcasts} = useGeneratePodcast(props);
+    
+    return (
+        <div>
+            <div className="flex flex-col gap-2.5">
+                <Label className='text-16 font-bold text-white-1'>Ai Prompt to generate Podcast</Label>
+                <Textarea className='input-class font-light focus-visible:ring-offset-orange-1'
+                    placeholder='Provide text to generate audio'
+                    rows={5}
+                    value={props.voicePrompt}
+                    onChange={(e) => props.setVoicePrompt(e.target.value)} />
+            </div>
+            <div className="mt-5 w-full max-w-[200px]">
+                <Button type="submit" className="text-16 w-full bg-orange-1 py-4 font-bold text-white-1">{isGenerating ? (<>Generating <Loader size={20} className="animate-spin" /></>) : ("Generate")}</Button>
+            </div>
+            {props.audio && (
+                <audio controls src={props.audio} autoPlay className="mt-5" onLoadedMetadata={(e) => props.setAudioDuration(e.currentTarget.duration)} />
+            )}
+        </div>
+    )
+}
+
+export default GeneratePodcast
