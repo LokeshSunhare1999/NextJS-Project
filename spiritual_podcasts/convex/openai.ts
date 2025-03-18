d60045cd13d44a6442dc15602b0488638f64d1db
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2025-03-18 23:57:41 +0530

    generate podcast configuration

--- spiritual_podcasts/convex/openai.ts
+++ spiritual_podcasts/convex/openai.ts
@@ -1,17 +1,19 @@
 import { action } from "./_generated/server";
 import { v } from "convex/values";
-import OpenAI from "openai"
+import OpenAI from "openai";
+import { SpeechCreateParams } from "openai/resources/audio/speech.mjs";
 
-const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 export const generateAudioAction = action({
   args: { input: v.string(), voice: v.string() },
-  handler: async (_, args) => {
+  handler: async (_, { voice, input }) => {
     const mp3 = await openai.audio.speech.create({
-        model: "tts-1",
-        voice: "alloy",
-        input: "Today is a wonderful day to build something people love!",
-      });
-    return "success";
+      model: "tts-1",
+      voice: voice as SpeechCreateParams["voice"],
+      input: input,
+    });
+    const buffer = await mp3.arrayBuffer();
+    return buffer;
   },
 });
