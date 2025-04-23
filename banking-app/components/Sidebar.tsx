cf24a8605dd224be3136915ca7b911d19b319669
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2025-04-24 00:29:10 +0530

    profile page

--- banking-app/components/Sidebar.tsx
+++ banking-app/components/Sidebar.tsx
@@ -8,8 +8,9 @@ import React from "react";
 
 const Sidebar = ({ user }: SiderbarProps) => {
   const pathname = usePathname();
+  const { audio } = useAudio();
   return (
-    <section className="sidebar">
+    <section className={cn("sidebar h-[calc(100vh-5px)]",{'h-[calc(100vh-130px)]': audio?.audioUrl})}>
       <nav className="flex flex-col gap-4">
         <Link href="/" className="mb-12 cursor-pointer items-center flex gap-2">
           <Image
--- spiritual_podcasts/components/LeftSidebar.tsx
+++ spiritual_podcasts/components/LeftSidebar.tsx
@@ -7,13 +7,15 @@ import { sidebarLinks } from "@/constants";
 import { usePathname, useRouter } from "next/navigation";
 import { SignedIn, SignedOut, useClerk } from "@clerk/nextjs";
 import { Button } from "./ui/button";
+import { useAudio } from "@/providers/AudioProvider";
 
 const LeftSidebar = () => {
   const pathname = usePathname();
   const router = useRouter();
   const { signOut } = useClerk();
+  const { audio } = useAudio();
   return (
-    <section className="left_sidebar">
+    <section className={cn("left_sidebar h-[calc(100vh-5px)]",{'h-[calc(100vh-130px)]': audio?.audioUrl})}>
       <nav className="flex flex-col gap-6 ">
         <Link
           href="/"
