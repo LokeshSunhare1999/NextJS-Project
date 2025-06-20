1bd4d6840869679a82dc1981f88f5136c10b8185
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2025-06-20 19:12:13 +0530

    update css

--- landing-page/src/components/sections/HeroSection.tsx
+++ landing-page/src/components/sections/HeroSection.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@/ui/button";
 import Header from "@/components/layout/Header";
 import Image from "next/image";
 
@@ -7,19 +8,19 @@ export default function HeroSection() {
   return (
     <section id="hero" className="relative h-screen w-full flex flex-col items-center overflow-hidden bg-black mt-0 pt-0">
       {/* Background image */}
-      <div className="absolute inset-0 bg-[url('/herobg.webp')] bg-cover bg-center bg-no-repeat">
-        {/* <Image
+      <div className="absolute inset-0">
+        <Image
           src="/herobg.webp"
-          alt="herobg"
+          alt="Hero background"
           fill
           priority
           sizes="100vw"
-          className="object-cover"
-        /> */}
+          className="object-cover absolute inset-0 z-0"
+        />
       </div>
 
       {/* Overlay gradient */}
-      <div className="absolute inset-0 bg-gradient-to-t from-black via-black/70 to-black/30 pointer-events-none" />
+      <div className="absolute inset-0 bg-gradient-to-t from-black via-black/70 to-black/30" />
 
       {/* Header */}
       <div className="w-full relative z-20">
@@ -27,7 +28,7 @@ export default function HeroSection() {
       </div>
 
       {/* Content */}
-      <div className="container min-h-[80vh] z-10 flex flex-col justify-end items-center text-white gap-3 sm:gap-5 pt-64 sm:pt-64">
+      <div className="container flex-1 z-10 flex flex-col justify-center items-center text-white gap-3 sm:gap-5 pt-32 sm:pt-40">
         <h1 className="text-4xl xs:text-2xl sm:text-3xl md:text-5xl lg:text-7xl font-bold mb-2 sm:mb-3 text-center font-['helvetica']">
           The Future of Hiring is Here
         </h1>
