1a3043eae6a79fa72fa1d0cba23ba9b464374ce0
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2025-06-20 18:29:34 +0530

    update css

--- landing-page/src/components/sections/HeroSection.tsx
+++ landing-page/src/components/sections/HeroSection.tsx
@@ -8,16 +8,15 @@ export default function HeroSection() {
   return (
     <section id="hero" className="relative h-screen w-full flex flex-col items-center overflow-hidden bg-black mt-0 pt-0">
       {/* Background image */}
-      <div className="absolute inset-0">
-        <Image
+      <div className="absolute inset-0 bg-[url('/herobg.webp')] bg-cover bg-center bg-no-repeat">
+        {/* <Image
           src="/herobg.webp"
-          alt="hero-bg"
+          alt="herobg"
           fill
           priority
           sizes="100vw"
-          placeholder="blur"
           className="object-cover"
-        />
+        /> */}
       </div>
 
       {/* Overlay gradient */}
@@ -29,16 +28,16 @@ export default function HeroSection() {
       </div>
 
       {/* Content */}
-      {/* <div className="container h-full pb-20 md:pb-15 relative z-10 px-4 sm:px-6 flex flex-col justify-end items-center text-white gap-3 sm:gap-5 pt-64 sm:pt-64">
-        <h1 className="text-4xl xs:text-2xl sm:text-3xl md:text-5xl lg:text-7xl font-bold mb-2 sm:mb-3 text-center font-['helvetica']">
-          The Future of Hiring is Here
-        </h1>
-      </div> */}
-      <div className="container min-h-[80vh] pb-20 relative z-10 px-4 sm:px-6 flex flex-col justify-end items-center text-white gap-3 sm:gap-5">
-        <h1 className="">
+      <div className="container h-full pb-20 md:pb-15 relative z-10 px-4 sm:px-6 flex flex-col justify-end items-center text-white gap-3 sm:gap-5 pt-64 sm:pt-64">
+        <h1 className="text-4xl xs:text-2xl sm:text-3xl md:text-5xl lg:text-7xl font-bold mb-2 sm:mb-3 text-center">
           The Future of Hiring is Here
         </h1>
       </div>
+      {/* <div className="container min-h-[80vh] pb-20 relative z-10 px-4 sm:px-6 flex flex-col justify-end items-center text-white gap-3 sm:gap-5">
+        <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold mb-2 sm:mb-3 text-center font-sans">
+          The Future of Hiring is Here
+        </h1>
+      </div> */}
     </section>
   );
 }
