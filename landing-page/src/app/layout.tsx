56220ca8d1c0063c7b6e696d5a4ebe6ecab5f4d5
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2025-06-21 23:42:23 +0530

    remove script

--- landing-page/src/app/layout.tsx
+++ landing-page/src/app/layout.tsx
@@ -28,13 +28,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script id="google-tag-manager" strategy="afterInteractive">
+        {/* <Script id="google-tag-manager" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-TTS7HWCR');`}
-        </Script>
+        </Script> */}
       </head>
       <body suppressHydrationWarning className={`antialiased ${poppins.variable} font-poppins`}>
         <noscript>
