7582b35f8f18facbdc56fb4e11cf34f08f6cc358
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2025-06-23 18:34:10 +0530

    remove bg-img

--- landing-page/src/components/sections/HeroSection.tsx
+++ landing-page/src/components/sections/HeroSection.tsx
@@ -5,10 +5,10 @@ import Image from "next/image";
 
 export default function HeroSection() {
   return (
-    <section id="hero" className="relative h-screen w-full flex flex-col items-center overflow-hidden bg-black mt-0 pt-0">
+    <section id="hero" className="relative bg-[url('/herobg.webp')] bg-cover bg-center bg-no-repeat h-screen w-full flex flex-col items-center overflow-hidden mt-0 pt-0">
       {/* Background image */}
-      <div className="absolute inset-0 bg-orange-600">
-        {/* <Image
+      {/* <div className="absolute inset-0 bg-orange-600">
+         <Image
           src="/herobg.webp"
           alt="Team working in futuristic hiring environment"
           fill
@@ -16,8 +16,8 @@ export default function HeroSection() {
           loading="lazy"
           sizes="100vw"
           className="object-cover"
-        /> */}
-      </div>
+        />
+      </div> */}
 
       {/* Overlay gradient */}
       <div className="absolute inset-0 bg-gradient-to-t from-black via-black/70 to-black/30" />
