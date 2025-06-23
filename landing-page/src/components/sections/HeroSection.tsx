9f954a38e0722bdcbc6e2be53b0d1b6676bf3851
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2025-06-23 13:11:58 +0530

    update bg img name

--- landing-page/src/components/sections/HeroSection.tsx
+++ landing-page/src/components/sections/HeroSection.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@/ui/button";
 import Header from "@/components/layout/Header";
 import Image from "next/image";
 
@@ -7,17 +8,14 @@ export default function HeroSection() {
   return (
     <section id="hero" className="relative h-screen w-full flex flex-col items-center overflow-hidden bg-black mt-0 pt-0">
       {/* Background image */}
-      <div className="absolute inset-0 bg-white z-0">
+      <div className="absolute inset-0">
         <Image
-          src="/heroBg.webp"
+          src="/herobg.webp"
           alt="Hero background"
           fill
           priority
-          quality={75}
-          placeholder="blur"
-          blurDataURL="/reelslogo.webp"
           sizes="100vw"
-          className="object-cover z-[-1] "
+          className="object-cover absolute inset-0 z-0"
         />
       </div>
 
@@ -35,6 +33,11 @@ export default function HeroSection() {
           The Future of Hiring is Here
         </h1>
       </div>
+      {/* <div className="container min-h-[80vh] pb-20 relative z-10 px-4 sm:px-6 flex flex-col justify-end items-center text-white gap-3 sm:gap-5">
+        <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold mb-2 sm:mb-3 text-center font-sans">
+          The Future of Hiring is Here
+        </h1>
+      </div> */}
     </section>
   );
 }
