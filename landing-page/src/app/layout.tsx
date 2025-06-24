42b6650b6693ba4a27c88b4239c353f313c0a92f
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2025-06-24 17:42:33 +0530

    update layout file

--- landing-page/src/app/layout.tsx
+++ landing-page/src/app/layout.tsx
@@ -1,5 +1,77 @@
+// import type { Metadata } from "next";
+// import "../../public/styles/globals.css";
+// import ClientBody from "./ClientBody";
+// import GoogleAnalytics from "./components/GoogleAnalytics";
+// import { Poppins } from 'next/font/google';
+// import Script from 'next/script';
+
+// const poppins = Poppins({
+//   subsets: ['latin'],
+//   display: 'swap',
+//   weight: ['300', '400', '500', '600'],
+//   variable: '--font-poppins',
+//   preload: true, // Preload the font
+// });
+
+// export const metadata: Metadata = {
+//   title: "Saathi",
+//   description: "Reel Banao, Naukari Paao",
+//   icons: {
+//     icon: "/Logo.svg",
+//   },
+// };
+// // import "../components/sections/IdentityVerified/IdentityVerified.css"
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en">
+//       <head>
+//         <link rel="preload" as="image" href="/herobg.webp" fetchPriority="high" />
+//         <link rel="preload" href="/fonts/helvetica-255/helvetica.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+//         <link rel="preload" href="/fonts/helvetica-255/helvetica-Bold.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+//         <link rel="preload" href="/fonts/helvetica-255/helvetica-Oblique.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+//         <link rel="preload" href="/fonts/helvetica-255/helvetica-BoldOblique.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+        
+//         <link  rel="preload" href="/styles/globals.css" as="style" onLoad="this.onload=null;this.rel='stylesheet';" />
+//         <link  rel="preload" href="/styles/IdentityVerified.css" as="style" onLoad="this.onload=null;this.rel='stylesheet';" />
+//         <link  rel="preload" href="/styles/JobReelContainer.css" as="style" onLoad="this.onload=null;this.rel='stylesheet';" />
+//         <link  rel="preload" href="/styles/MobileScreenStyles.css" as="style" onLoad="this.onload=null;this.rel='stylesheet';" />
+
+//         <noscript>
+//           <link rel="stylesheet" href="/styles/globals.css" />
+//           <link rel="stylesheet" href="/styles/IdentityVerified.css" />
+//           <link rel="stylesheet" href="/styles/JobReelContainer.css" />
+//           <link rel="stylesheet" href="/styles/MobileScreenStyles.css" />
+//         </noscript>
+
+//         <Script id="google-tag-manager" strategy="lazyOnload">
+//           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+//           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+//           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+//           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+//           })(window,document,'script','dataLayer','GTM-TTS7HWCR');`}
+//         </Script>
+//       </head>
+//       <body suppressHydrationWarning className={`antialiased ${poppins.variable} font-poppins`}>
+//         <noscript>
+//           <iframe
+//             src="https://www.googletagmanager.com/ns.html?id=GTM-TTS7HWCR"
+//             height="0"
+//             width="0"
+//             style={{ display: 'none', visibility: 'hidden' }}
+//           />
+//         </noscript>
+//         <GoogleAnalytics />
+//         <ClientBody>{children}</ClientBody>
+//       </body>
+//     </html>
+//   );
+// }
+
 import type { Metadata } from "next";
-import "../../public/styles/globals.css";
 import ClientBody from "./ClientBody";
 import GoogleAnalytics from "./components/GoogleAnalytics";
 import { Poppins } from 'next/font/google';
@@ -8,9 +80,9 @@ import Script from 'next/script';
 const poppins = Poppins({
   subsets: ['latin'],
   display: 'swap',
-  weight: ['300', '400', '500', '600', '700'],
+  weight: ['400', '500', '600'], // Reduce font weights to only what you need
   variable: '--font-poppins',
-  preload: true, // Preload the font
+  preload: true,
 });
 
 export const metadata: Metadata = {
@@ -20,7 +92,7 @@ export const metadata: Metadata = {
     icon: "/Logo.svg",
   },
 };
-// import "../components/sections/IdentityVerified/IdentityVerified.css"
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -29,31 +101,70 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Critical resource preloads */}
         <link rel="preload" as="image" href="/herobg.webp" fetchPriority="high" />
-        <link rel="preload" href="/fonts/helvetica-255/helvetica.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
-        <link rel="preload" href="/fonts/helvetica-255/helvetica-Bold.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
-        <link rel="preload" href="/fonts/helvetica-255/helvetica-Oblique.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
-        <link rel="preload" href="/fonts/helvetica-255/helvetica-BoldOblique.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
         
-        <link media="print" href="/styles/globals.css" as="style" onLoad="this.onload=null;this.rel='stylesheet';" />
-        <link media="print" href="/styles/IdentityVerified.css" as="style" onLoad="this.onload=null;this.rel='stylesheet';" />
-        <link media="print" href="/styles/JobReelContainer.css" as="style" onLoad="this.onload=null;this.rel='stylesheet';" />
-        <link media="print" href="/styles/MobileScreenStyles.css" as="style" onLoad="this.onload=null;this.rel='stylesheet';" />
+        {/* Font preloads - only preload critical fonts */}
+        <link 
+          rel="preload" 
+          href="/fonts/helvetica-255/helvetica.ttf" 
+          as="font" 
+          type="font/ttf" 
+          crossOrigin="anonymous" 
+        />
+        <link 
+          rel="preload" 
+          href="/fonts/helvetica-255/helvetica-Bold.ttf" 
+          as="font" 
+          type="font/ttf" 
+          crossOrigin="anonymous" 
+        />
+        
+        {/* Critical CSS - Load synchronously for FCP */}
+        <link rel="stylesheet" href="/styles/globals.css" />
+        
+        {/* Non-critical CSS - Load asynchronously */}
+        <link 
+          rel="preload" 
+          href="/styles/IdentityVerified.css" 
+          as="style" 
+          onLoad={(e) => {
+            const target = e.target as HTMLLinkElement;
+            target.onload = null;
+            target.rel = 'stylesheet';
+          }}
+        />
+        <link 
+          rel="preload" 
+          href="/styles/JobReelContainer.css" 
+          as="style" 
+          onLoad={(e) => {
+            const target = e.target as HTMLLinkElement;
+            target.onload = null;
+            target.rel = 'stylesheet';
+          }}
+        />
+        <link 
+          rel="preload" 
+          href="/styles/MobileScreenStyles.css" 
+          as="style" 
+          onLoad={(e) => {
+            const target = e.target as HTMLLinkElement;
+            target.onload = null;
+            target.rel = 'stylesheet';
+          }}
+        />
 
+        {/* Fallback for no-JS */}
         <noscript>
-          <link rel="stylesheet" href="/styles/globals.css" />
           <link rel="stylesheet" href="/styles/IdentityVerified.css" />
           <link rel="stylesheet" href="/styles/JobReelContainer.css" />
           <link rel="stylesheet" href="/styles/MobileScreenStyles.css" />
         </noscript>
 
-        <Script id="google-tag-manager" strategy="lazyOnload">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-TTS7HWCR');`}
-        </Script>
+        {/* DNS prefetch for external resources */}
+        <link rel="dns-prefetch" href="//www.googletagmanager.com" />
+        <link rel="dns-prefetch" href="//www.google-analytics.com" />
       </head>
       <body suppressHydrationWarning className={`antialiased ${poppins.variable} font-poppins`}>
         <noscript>
@@ -64,8 +175,18 @@ export default function RootLayout({
             style={{ display: 'none', visibility: 'hidden' }}
           />
         </noscript>
+        
         <GoogleAnalytics />
         <ClientBody>{children}</ClientBody>
+        
+        {/* Move GTM to end of body for better FCP */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TTS7HWCR');`}
+        </Script>
       </body>
     </html>
   );
