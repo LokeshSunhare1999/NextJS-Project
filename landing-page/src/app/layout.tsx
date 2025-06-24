db58b5d09ac597babe25a35bffb67b2b02fa145c
ils-lokeshs <98936997+ils-lokeshs@users.noreply.github.com>
2025-06-24 18:39:41 +0530

    inline

--- landing-page/src/app/layout.tsx
+++ landing-page/src/app/layout.tsx
@@ -44,7 +44,79 @@ export default function RootLayout({
           <link rel="stylesheet" href="/styles/JobReelContainer.css" />
           <link rel="stylesheet" href="/styles/MobileScreenStyles.css" />
         </noscript> */}
+<Script
+          id="css-loader"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              // Function to load CSS asynchronously
+              function loadCSS(href, before, media) {
+                var doc = window.document;
+                var ss = doc.createElement("link");
+                var ref;
+                if (before) {
+                  ref = before;
+                } else {
+                  var refs = (doc.body || doc.getElementsByTagName("head")[0]).childNodes;
+                  ref = refs[refs.length - 1];
+                }
+                var sheets = doc.styleSheets;
+                ss.rel = "stylesheet";
+                ss.href = href;
+                ss.media = "only x";
+                function ready(cb) {
+                  if (doc.body) {
+                    return cb();
+                  }
+                  setTimeout(function() {
+                    ready(cb);
+                  });
+                }
+                ready(function() {
+                  ref.parentNode.insertBefore(ss, (before ? ref : ref.nextSibling));
+                });
+                var onloadcssdefined = function(cb) {
+                  var resolvedHref = ss.href;
+                  var i = sheets.length;
+                  while (i--) {
+                    if (sheets[i].href === resolvedHref) {
+                      return cb();
+                    }
+                  }
+                  setTimeout(function() {
+                    onloadcssdefined(cb);
+                  });
+                };
+                function loadCB() {
+                  if (ss.addEventListener) {
+                    ss.removeEventListener("load", loadCB);
+                  }
+                  ss.media = media || "all";
+                }
+                if (ss.addEventListener) {
+                  ss.addEventListener("load", loadCB);
+                }
+                ss.onloadcssdefined = onloadcssdefined;
+                onloadcssdefined(loadCB);
+                return ss;
+              }
 
+              // Load non-critical CSS after page load
+              window.addEventListener('load', function() {
+                // Only load if not already loaded via preload
+                if (!document.querySelector('link[href="/styles/IdentityVerified.css"][rel="stylesheet"]')) {
+                  loadCSS('/styles/IdentityVerified.css');
+                }
+                if (!document.querySelector('link[href="/styles/JobReelContainer.css"][rel="stylesheet"]')) {
+                  loadCSS('/styles/JobReelContainer.css');
+                }
+                if (!document.querySelector('link[href="/styles/MobileScreenStyles.css"][rel="stylesheet"]')) {
+                  loadCSS('/styles/MobileScreenStyles.css');
+                }
+              });
+            `
+          }}
+        />
         <Script id="google-tag-manager" strategy="lazyOnload">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
