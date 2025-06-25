d97e9f1fae1763f15458cec95b3094b793068a63
ils-lokeshs <98936997+ils-lokeshs@users.noreply.github.com>
2025-06-25 11:16:57 +0530

    positioned GTM

--- landing-page/src/app/layout.tsx
+++ landing-page/src/app/layout.tsx
@@ -30,14 +30,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preload" as="image" href="/herobg.webp" fetchPriority="high" />
-        
-        <Script id="google-tag-manager" strategy="lazyOnload">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-TTS7HWCR');`}
-        </Script>
+      
       </head>
       <body suppressHydrationWarning className={`antialiased ${poppins.variable} font-poppins`}>
         <noscript>
@@ -50,6 +43,13 @@ export default function RootLayout({
         </noscript>
         <GoogleAnalytics />
         <ClientBody>{children}</ClientBody>
+          <Script id="google-tag-manager" strategy="lazyOnload">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TTS7HWCR');`}
+        </Script>
       </body>
     </html>
   );
